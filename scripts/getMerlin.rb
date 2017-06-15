01327cf89a3878e94e7372feef891d12e23f695a
Florian v.Schoenebeck <f_schoenebeck@gmx.net>
2017-06-15 19:32:17 +0200

    vue2 example

--- scripts/getMerlin.rb
+++ scripts/getMerlin.rb
@@ -2,19 +2,20 @@
 # getMerlin.rb
 # ******************************************************************************** 
 require 'open-uri'
-
 def board(nr)
-  homepage = "http://www.bridge-verband.de/images/uploads/ftp3390/Bundesligen/"
+  #homepage = "http://www.bridge-verband.de/images/uploads/ftp3390/Bundesligen/"
+  homepage = "http://www.bridge-suedbayern.de/2016/IBPM/EndErg/"
   #homepage = "http://www.bridgeclubkirchzarten.de/ergebnisse2016/
-  bezeichner = "16_10_29_wer_bdz_k1_d5_bd"#
-  #bezeichner = "16_10_22_bez_bdz_k0_d2_bd"#
+  #bezeichner = "16_11_11_bez_bdz_k2_bd"
+  bezeichner = "16_11_11_bez_bdz_k0_d2_bd"
+  #bezeichner = "16_10_22_bez_bdz_k0_d2_bd"
   url = "#{homepage}#{bezeichner}#{nr}.html"
   page = open(url)
   page.read
 end
 
 if $0 == __FILE__ 
-    for nr in 1..15 do 
+    for nr in 1..26 do 
         file = open("board_" + nr.to_s + ".html", 'w')
         file.write(board(nr))
         file.close
--- scripts/insert_2_boards.bat
+++ scripts/insert_2_boards.bat
@@ -1,1 +1,1 @@
-curl -d @titi_1023.json -X POST http://admin:passw0rd@127.0.0.1:5984/boards/_bulk_docs -H Content-Type:application/json
+curl -d @coeur.json -X POST http://admin:passw0rd@127.0.0.1:5984/boards/_bulk_docs -H Content-Type:application/json
